6dc05cb6f760b595f7f4a52f422c88f9b0e5b2d8
NARKO-KROL <osipddddov048@mail.ru>
2022-03-24 06:54:04 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xAa0BfD493085631927348019A0568ec1f086F97e"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
